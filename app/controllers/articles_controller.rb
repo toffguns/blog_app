57ba6a74b2c9974245b99fd8553c1b3880e53e84
toffguns <dugchun@aol.com>
2017-09-01 23:55:16 +0000

    create article successfully

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,22 @@
 class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+ 
   def index
   end
+  
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash[:success] = "Article has been created"
+    redirect_to articles_path
+  end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+  
+  
 end
