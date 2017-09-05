b99371474f5ec703779f243c6dacddd2c07851fb
toffguns <dugchun@aol.com>
2017-09-05 23:56:13 +0000

    Implement show article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
